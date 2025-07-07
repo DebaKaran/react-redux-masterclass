19c0b61c0c96ff1992097a65edcf3086c169716d
DebaKaran <karan.deba@gmail.com>
2025-07-07 22:22:16 +0530

    feat: add AnimalShow component with random animal generator and list rendering

---
+++ animals/src/components/AnimalShow.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+// interface AnimalShowProps {
+//   animal: string;
+// }
+
+const getRandomAnimal = (): string => {
+  const animals: string[] = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  const randomNum: number = Math.floor(Math.random() * animals.length);
+
+  return animals[randomNum];
+};
+
+const AnimalShow = () => {
+  const [animals, setAnimals] = useState<string[]>([]);
+
+  const handleOnClick = () => {
+    const animal = getRandomAnimal();
+    setAnimals([...animals, animal]);
+  };
+
+  return (
+    <>
+      <button onClick={handleOnClick}>Add Animal</button>
+      {animals.map((animal, index) => (
+        <div key={index}>{animal}</div>
+      ))}
+    </>
+  );
+};
+
+export default AnimalShow;
