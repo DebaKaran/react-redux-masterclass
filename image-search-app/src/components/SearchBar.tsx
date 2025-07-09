4e622e2670b4fd3b5d9bef5150009f7b8279b581
DebaKaran <karan.deba@gmail.com>
2025-07-09 13:48:09 +0530

    feat: implement image search flow with controlled input and async fetch - Added controlled input handling in SearchBar with onKeyDown for "Enter" key - Defined handleSearch in App to fetch images using fetchImages - Used useState to manage and display image descriptions in App

--- image-search-app/src/components/SearchBar.tsx
+++ image-search-app/src/components/SearchBar.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const SearchBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
 }
 
-export default SearchBar
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [term, setTerm] = useState("");
+
+  const handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSearch(term);
+    }
+  };
+
+  return (
+    <div>
+      <label> Search Term</label>
+      <input
+        type="text"
+        onChange={(e) => setTerm(e.target.value)}
+        onKeyDown={handleOnKeyDown}
+        value={term}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
