e39c1f574e373b2bcc922377960476124209f5c5
DebaKaran <karan.deba@gmail.com>
2025-07-09 23:19:46 +0530

    feat: add BookList and BookShow components to render list of books
    - Created BookList to map and render each book
    - Created BookShow to display individual book title
    - Integrated both into App component

--- books/src/components/BookList.tsx
+++ books/src/components/BookList.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import type { Book } from "../types/Book";
+import BookShow from "./BookShow";
 
-const BookList = () => {
-  return <div></div>;
+interface BookListProps {
+  books: Book[];
+}
+const BookList: React.FC<BookListProps> = ({ books }) => {
+  const renderedBooks = books.map((book) => {
+    return <BookShow key={book.id} book={book} />;
+  });
+  return <div className="book-list">{renderedBooks}</div>;
 };
 
 export default BookList;
