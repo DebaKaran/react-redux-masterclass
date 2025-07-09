623112001eb672191dcf4b68402c458e0c1be695
DebaKaran <karan.deba@gmail.com>
2025-07-09 12:14:47 +0530

    feat: integrate Unsplash API to fetch and log images using environment variable for access key - Added fetchImages utility in `unsplash-api.ts` to call Unsplash search API - Secured API key using `.env` and Vite's `import.meta.env` - Invoked fetchImages in `App.tsx` inside useEffect and logged results - Validated response structure and cleaned data before use - Updated `.gitignore` to prevent `.env` from being tracked - `package.json` and `package-lock.json` updated due to axios installation

--- image-search-app/src/App.tsx
+++ image-search-app/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import { fetchImages, type UnsplashImage } from "./api/unsplash-api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const loadImages = async () => {
+      try {
+        const images: UnsplashImage[] = await fetchImages("car");
+        console.log("Fetched images from API:", images);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+      }
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    loadImages();
+  }, []);
+  return <>Learn React</>;
 }
 
-export default App
+export default App;
