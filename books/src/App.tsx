9dd93575d7186271174a642a50dcc92f8ecda286
DebaKaran <karan.deba@gmail.com>
2025-07-09 20:59:23 +0530

    feat: render list of books in App component using map - Implemented dynamic rendering of books with title and id - Corrected JSX syntax for mapping over books array - Ensured each item has a unique key using book.id - Temporarily displaying book list directly in App before integrating BookList component

--- books/src/App.tsx
+++ books/src/App.tsx
@@ -1,11 +1,24 @@
-t './App.css'
+import { useState } from "react";
+import type { Book } from "./types/Book";
+import BookCreate from "./components/BookCreate";
 
 function App() {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  const handleAddBook = (book: Book) => {
+    setBooks((prev) => [...prev, book]);
+  };
+
   return (
     <>
-      
+      <BookCreate numberOfBooks={books.length} addBook={handleAddBook} />
+      {books.map((book) => (
+        <div key={book.id}>
+          {book.id}, {book.title}
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
