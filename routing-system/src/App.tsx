26cfd521d17da1ffd90d98ab74510d3dcf2231d2
DebaKaran <karan.deba@gmail.com>
2025-07-12 19:31:00 +0530

    feat: add react-icons library and integrate icons in Button usage in App.tsx

--- routing-system/src/App.tsx
+++ routing-system/src/App.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import "./App.css";
 import Button from "./components/Buttons/Button";
+import { GoBell, GoDatabase, GoCloud } from "react-icons/go";
 
 function App() {
   return (
@@ -11,16 +12,19 @@ function App() {
       <div className="bg-blue-500 text-white p-4">Tailwind Working?</div>
 
       <Button buttonVariantProps={{ primary: true }} rounded outlined>
+        <GoBell />
         Clicked Me!!
       </Button>
       <br />
       <Button buttonVariantProps={{ danger: true }} outlined>
+        <GoCloud />
         Buy Now
       </Button>
       <br />
       <Button buttonVariantProps={{ warning: true }}>See Deal</Button>
       <br />
       <Button buttonVariantProps={{ secondary: true }} outlined>
+        <GoDatabase />
         Hide Ads!
       </Button>
       <br />
--- routing-system/src/components/Buttons/Button.tsx
+++ routing-system/src/components/Buttons/Button.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { ButtonVariantProps } from "./button-types";
 import className from "classnames";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
   buttonVariantProps: ButtonVariantProps;
@@ -9,19 +10,33 @@ interface ButtonProps {
   children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ buttonVariantProps, children }) => {
+const Button: React.FC<ButtonProps> = ({
+  buttonVariantProps,
+  outlined,
+  rounded,
+  children,
+}) => {
   // Convert variant object to string
   const variant = Object.keys(
     buttonVariantProps
   )[0] as keyof ButtonVariantProps;
 
-  const classes = className("px-6 py-3 border", {
-    "bg-blue-500 border-blue-500 text-white": variant === "primary",
-    "bg-gray-900 border-gray-900 text-white": variant === "secondary",
-    "bg-green-500 border-green-500 text-white": variant === "success",
-    "bg-yellow-700 border-yellow-700 text-white": variant === "warning",
-    "bg-red-500 border-red-500 text-white": variant === "danger",
-  });
+  const classes = twMerge(
+    className("px-3 py-1.5 border", {
+      "border-blue-500 bg-blue-500 text-white": variant === "primary",
+      "border-gray-900 bg-gray-900 text-white": variant === "secondary",
+      "border-green-500 bg-green-500 text-white": variant === "success",
+      "border-yellow-400 bg-yellow-400 text-white": variant === "warning",
+      "border-red-500 bg-red-500 text-white": variant === "danger",
+      "rounded-full": rounded,
+      "bg-white": outlined,
+      "text-blue-500": outlined && variant === "primary",
+      "text-gray-900": outlined && variant === "secondary",
+      "text-green-500": outlined && variant === "success",
+      "text-yellow-400": outlined && variant === "warning",
+      "text-red-500": outlined && variant === "danger",
+    })
+  );
   return <button className={classes}>{children}</button>;
 };
 
