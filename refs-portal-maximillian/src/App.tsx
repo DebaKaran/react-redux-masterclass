44fb0ba4a9cbbd353e47051e7cc8eb26fd9bf1c4
DebaKaran <karan.deba@gmail.com>
2025-07-19 11:33:13 +0530

    feat: implement TimerChallenge component with title, timer logic, and status display

--- refs-portal-maximillian/src/App.tsx
+++ refs-portal-maximillian/src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
 
 function App() {
   return (
     <>
       <Player />
+      <div id="challenges">
+        <TimerChallenge title="EASY" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting Tough" targetTime={10} />
+        <TimerChallenge title="Pros Only" targetTime={15} />
+      </div>
     </>
   );
 }
---
+++ refs-portal-maximillian/src/components/TimerChallenge.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+interface TimerChallengeProps {
+  title: string;
+  targetTime: number;
+}
+
+const TimerChallenge: React.FC<TimerChallengeProps> = ({
+  title,
+  targetTime,
+}) => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const handleStart = () => {
+    if (isRunning || timerExpired) return; // prevent multiple starts
+    setIsRunning(true);
+    setTimeout(() => {
+      setTimerExpired(true);
+      setIsRunning(false);
+    }, targetTime * 1000);
+  };
+
+  const timeText = `${targetTime} ${targetTime === 1 ? "second" : "seconds"}`;
+  const statusText = timerExpired
+    ? "Time is finished"
+    : isRunning
+    ? "Time is running"
+    : "Timer inactive";
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost</p>}
+      <p className="challenge-time">{timeText}</p>
+      <p>
+        <button onClick={handleStart} disabled={isRunning || timerExpired}>
+          Start Challenge
+        </button>
+      </p>
+      <p>{statusText}</p>
+    </section>
+  );
+};
+
+export default TimerChallenge;
