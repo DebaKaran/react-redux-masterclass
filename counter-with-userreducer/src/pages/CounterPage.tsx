c9e34f7abc04f05bcabeb10c6ff6a4f9c9174b8d
DebaKaran <karan.deba@gmail.com>
2025-07-23 19:50:39 +0530

    Initial setup for reducer

---
+++ counter-with-userreducer/src/pages/CounterPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { Count } from "../types/count";
+import useCounter from "../hooks/useCounter";
+
+const CounterPage: React.FC<Count> = ({ initialCount }) => {
+  const { count, increment } = useCounter(initialCount);
+  return (
+    <div>
+      <h1>Count is: {count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
+
+export default CounterPage;
---
+++ counter-with-userreducer/src/types/Count.ts
@@ -0,0 +1,3 @@
+export type Count = {
+  initialCount: number;
+};
