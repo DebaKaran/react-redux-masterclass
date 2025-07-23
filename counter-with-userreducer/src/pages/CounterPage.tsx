10a7d6f2d9fc0491634a10c55867f95b6e561df9
DebaKaran <karan.deba@gmail.com>
2025-07-24 00:37:09 +0530

    refactor: convert CounterPage to useReducer for state management
    - Replaced useState with useReducer to manage count and input value state
    - Defined State and Action types for reducer
    - Introduced action constants and reducer function
    - Improved input handling and form submission logic

--- counter-with-userreducer/src/pages/CounterPage.tsx
+++ counter-with-userreducer/src/pages/CounterPage.tsx
@@ -1,40 +1,79 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import type { Count } from "../types/count";
-import useCounter from "../hooks/useCounter";
+
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const SET_VALUE_TO_ADD = "change-value-to-add";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
+
+type State = {
+  count: number;
+  valueToAdd: number;
+};
+
+type Action =
+  | { type: "increment" }
+  | { type: "decrement" }
+  | { type: "change-value-to-add"; payload: number }
+  | { type: "add_value_to_count" };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT_COUNT:
+      return { ...state, count: state.count - 1 };
+    case SET_VALUE_TO_ADD:
+      return { ...state, valueToAdd: action.payload };
+    case ADD_VALUE_TO_COUNT:
+      return { ...state, count: state.count + state.valueToAdd, valueToAdd: 0 };
+  }
+  return state;
+};
 
 const CounterPage: React.FC<Count> = ({ initialCount }) => {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  const initalState = { count: initialCount, valueToAdd: 0 };
+
+  const [state, dispatch] = useReducer(reducer, initalState);
 
   const increment = () => {
-    setCount((prevCount) => prevCount + 1);
+    const incrementAction: Action = { type: INCREMENT_COUNT };
+    dispatch(incrementAction);
   };
 
   const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
+    const decrementAction: Action = { type: DECREMENT_COUNT };
+    dispatch(decrementAction);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseInt(event.target.value);
-    setValueToAdd(value);
-    console.log(typeof value);
+    //If user types an empty string, parseInt("") returns NaN.
+    const value = parseInt(event.target.value) || 0;
+    const changeAction: Action = { type: SET_VALUE_TO_ADD, payload: value };
+    dispatch(changeAction);
   };
 
-  const hanldeSubmit = (event: React.FormEvent) => {
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    setCount((count) => count + valueToAdd);
-    setValueToAdd(0);
+    const addValueToCountAction: Action = { type: ADD_VALUE_TO_COUNT };
+    dispatch(addValueToCountAction);
+    // setCount((count) => count + valueToAdd);
+    // setValueToAdd(0);
   };
   return (
     <div>
-      <h1>Count is: {count}</h1>
+      <h1>Count is: {state.count}</h1>
       <div>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
       </div>
-      <form onSubmit={hanldeSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
-        <input type="number" value={valueToAdd || ""} onChange={handleChange} />
+        <input
+          type="number"
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+        />
         <button>Add it!!</button>
       </form>
     </div>
