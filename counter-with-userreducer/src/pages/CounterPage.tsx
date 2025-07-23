882c80fe5f71b146d6cf7e5c394e60f7265e837e
DebaKaran <karan.deba@gmail.com>
2025-07-23 20:49:59 +0530

    feat: implement counter page using useState with increment, decrement, and dynamic add functionality

--- counter-with-userreducer/src/pages/CounterPage.tsx
+++ counter-with-userreducer/src/pages/CounterPage.tsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import type { Count } from "../types/count";
 import useCounter from "../hooks/useCounter";
 
 const CounterPage: React.FC<Count> = ({ initialCount }) => {
-  const { count, increment } = useCounter({ initialCount });
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
+    setValueToAdd(value);
+    console.log(typeof value);
+  };
+
+  const hanldeSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setCount((count) => count + valueToAdd);
+    setValueToAdd(0);
+  };
   return (
     <div>
       <h1>Count is: {count}</h1>
-      <button onClick={increment}>Increment</button>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+      <form onSubmit={hanldeSubmit}>
+        <label>Add a lot!</label>
+        <input type="number" value={valueToAdd || ""} onChange={handleChange} />
+        <button>Add it!!</button>
+      </form>
     </div>
   );
 };
